aa37e8881f4b0b8980003f5f7ff6e92301f3d3e6
Felipe M <philmes@gmail.com>
2021-03-29 23:48:51 -0300

    basic port

--- nodejs_data_demo.js
+++ nodejs_data_demo.js
@@ -2,7 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
-const port = 3000;
+//const port = 3000;
+const port = process.env.PORT || 4000
 
 // body-parser configuration
 app.use(bodyParser.urlencoded({ extended: true }));
